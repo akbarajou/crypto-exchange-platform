90aee38b86105ccc7cb5c3092d6aaee7826c2c63
Akbar <202290398@ajou.uz>
2025-01-29 13:09:26 +0500

    Add Wallet class with header and implementation files

---
+++ include/Wallet.h
@@ -0,0 +1,16 @@
+//
+// Created by akbar on 1/26/25.
+//
+
+#ifndef WALLET_H
+#define WALLET_H
+
+
+
+class Wallet {
+
+};
+
+
+
+#endif //WALLET_H
---
+++ src/Wallet.cpp
@@ -0,0 +1,5 @@
+//
+// Created by akbar on 1/26/25.
+//
+
+#include "Wallet.h"
